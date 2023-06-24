44de019a84d9fd6c0583aa010794e30a07bf8b68
GrasonMC <90360132+Grant-JHP@users.noreply.github.com>
2023-06-24 20:40:47 +1000

    Update Credits

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ export default {
     root: './src',
     publicDir: '../public',
     build: { outDir: '../dist' },
-    base: '//' //GitHub Project Name: /Project-On-GitHub/
+    base: '/Tree-Eats/'
 }
